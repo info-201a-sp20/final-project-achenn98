abb870224a3abfe40b0a22cd8359614a02e1daaa
achenn98 <Aiyuuuu@Aiyus-MacBook-Air.local>
2020-05-21 16:22:13 -0700

    replaced data folder

--- chart_one.R
+++ chart_one.R
@@ -1,14 +1,11 @@
 #set-up
 library("dplyr")
 library("leaflet")
-abortion_data <- read.csv("data/.csv", stringsAsFactors = F)
-states_regions <- read.csv("data/states_regions.csv", stringsAsFactors = F)
+abortion_data <- read.csv("testing/states_regions.csv", stringsAsFactors = F)
+wstates_regions <- read.csv("data/states_regions.csv", stringsAsFactors = F)
 
 #density map that shows the number of abortion clinics per region
 
 #binding states dataframe with regions dataframe
 region_grouped <- full_join(abortion_data, states_regions,
                             by = c("State" = "state_name"))
-
-
-testing
