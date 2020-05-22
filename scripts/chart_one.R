be2fddba3e6c05db01b156a9ed35f0a5b6a24751
achenn98 <Aiyuuuu@Aiyus-MacBook-Air.local>
2020-05-21 17:50:04 -0700

    finished chart 1, created folders

--- scripts/chart_one.R
+++ scripts/chart_one.R
@@ -1,6 +1,6 @@
 #set-up
 library("dplyr")
-library("leaflet")
+library("ggplot2")
 abortion_data <- read.csv("data/guttmacher_abortion_data.csv", stringsAsFactors = F)
 states_regions <- read.csv("data/states_regions.csv", stringsAsFactors = F)
 
@@ -16,3 +16,11 @@ region_grouped <- joined_df %>%
   summarise(total_abortion_clinics = sum(new_num_abortion_clinics, na.rm = T))
 
 #bar graph
+abortion_clinics_per_region <- function(df) {
+  graph <-   ggplot(data = df) +
+    geom_col(mapping = aes(x = Region, y = total_abortion_clinics)) +
+    labs(title = "Number Of Abortion Clinics Per Region",
+         x = "Region",
+         y = "Total Abortion Clinics")
+  return(graph)
+}
