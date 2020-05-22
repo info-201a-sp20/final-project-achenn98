6fe3218d85787faf4867bf02c7840df3761a6ae9
achenn98 <Aiyuuuu@Aiyus-MacBook-Air.local>
2020-05-21 17:23:29 -0700

    worked on chart one

--- chart_one.R
+++ chart_one.R
@@ -4,7 +4,15 @@ library("leaflet")
 abortion_data <- read.csv("data/guttmacher_abortion_data.csv", stringsAsFactors = F)
 states_regions <- read.csv("data/states_regions.csv", stringsAsFactors = F)
 
-#density map that shows the number of abortion clinics per region
+#bar graph shows the number of abortion clinics per region
 
 #binding states dataframe with regions dataframe
-region_grouped <- full_join(abortion_data, states_regions)
+joined_df <- full_join(abortion_data, states_regions)
+
+#grouping based on region
+region_grouped <- joined_df %>%
+  mutate(new_num_abortion_clinics = as.numeric(joined_df$Num_of_abortion_clinics_2017)) %>%
+  group_by(Region) %>%
+  summarise(total_abortion_clinics = sum(new_num_abortion_clinics, na.rm = T))
+
+#bar graph
