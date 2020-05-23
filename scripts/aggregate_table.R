9bf1f8a183f1fbe0b7377da4d1f6bbde0e4d5f93
achenn98 <Aiyuuuu@Aiyus-MacBook-Air.local>
2020-05-23 02:48:20 -0700

    merge

--- scripts/aggregate_table.R
+++ scripts/aggregate_table.R
@@ -7,8 +7,7 @@ states_regions <- read.csv("data/states_regions.csv", stringsAsFactors = F)
 #binding states dataframe with regions dataframe
 joined_df <- full_join(abortion_data, states_regions)
 joined_df <- joined_df[-c(52, 53, 54), ]
- View(joined_df)
- 
+
 summary_table <- joined_df %>%
   mutate(Abortion_rate
         = as.numeric(joined_df$Abortion.rate..the.no..of.abortions.per.1.000.women.aged.15.44...by.state.of.occurrence..2017), 
@@ -19,4 +18,3 @@ summary_table <- joined_df %>%
             contraception_percent = mean(contraception_percent, na.rm = T),
             Num_of_abortion_clinics_2017 = sum(Num_of_abortion_clinics_2017, na.rm = T))
 
-summary_table
