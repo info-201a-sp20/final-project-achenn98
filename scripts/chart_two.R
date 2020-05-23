d7869812d20989c6bc4f9ed4cbf60737d58f5f26
achenn98 <Aiyuuuu@Aiyus-MacBook-Air.local>
2020-05-22 21:57:49 -0700

    style

--- scripts/chart_two.R
+++ scripts/chart_two.R
@@ -1,27 +1,30 @@
 library("dplyr")
 library("ggplot2")
-abortion_data <- read.csv("data/guttmacher_abortion_data.csv", stringsAsFactors = F)
+abortion_data <- read.csv("data/guttmacher_abortion_data.csv",
+                          stringsAsFactors = F)
 states_regions <- read.csv("data/states_regions.csv", stringsAsFactors = F)
 
 #binding states dataframe with regions dataframe
 joined_df <- full_join(abortion_data, states_regions)
-joined_df<-  joined_df[-c(52, 53, 54),]
+joined_df <- joined_df[-c(52, 53, 54), ]
 
 #grouping abortion rate and number of clinics based on region
-Abortion_rate_and_clinics <- joined_df %>%
-  mutate(new_num_abortion_clinics = as.numeric(joined_df$Num_of_abortion_clinics_2017),
-    new_abortion_rate = as.numeric(joined_df$Abortion.rate..the.no..of.abortions.per.1.000.women.aged.15.44...by.state.of.occurrence..2017)) %>%
+abortion_rate_and_clinics <- joined_df %>%
+  mutate(new_num_abortion_clinics =
+           as.numeric(joined_df$Num_of_abortion_clinics_2017),
+    new_abortion_rate =
+      as.numeric(joined_df$Abortion.rate..the.no..of.abortions.per.1.000.women.aged.15.44...by.state.of.occurrence..2017)) %>%
   group_by(Region) %>%
-  summarize(total_abortion_clinics = sum(new_num_abortion_clinics, na.rm = T), 
-           total_abortion_rate = 
-              sum(new_abortion_rate, na.rm =T))
+  summarize(total_abortion_clinics = sum(new_num_abortion_clinics, na.rm = T),
+           total_abortion_rate =
+              sum(new_abortion_rate, na.rm = T))
 
 scatterplot <- function(df) {
   plot <- ggplot(data = df) +
-  geom_point(mapping = aes(x = total_abortion_clinics, y = total_abortion_rate, color = Region)) +
+  geom_point(mapping = aes(x = total_abortion_clinics, y = total_abortion_rate,
+                           color = Region)) +
   labs(title = "Total Abortion Rates x Total Abortion Clinics by Region",
-       x = "Total Abortion Clinics", 
+       x = "Total Abortion Clinics",
        y = "Total Abortion Rates")
   return(plot)
 }
-
