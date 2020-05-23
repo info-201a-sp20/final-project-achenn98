639c5310e21ae3b03985fcc28c2918971f448525
Kendyl Bouret <kendylb2@uw.edu>
2020-05-22 20:59:17 -0800

    Chart Three's Code

---
+++ scripts/Char_Three.R
@@ -0,0 +1,74 @@
+#Scatterplot of % of women on contraceptives VS 
+## of abortion clincs grouped by region
+
+#Loading Libraries
+
+library(dplyr)
+library(ggplot2)
+
+#Reading CSV's
+
+Resources_csv <- read.csv('guttmacher_abortion_data.csv', 
+                          stringsAsFactors = FALSE)
+Region_csv <- read.csv('states_regions.csv', stringsAsFactors
+                       = FALSE)
+
+#Combinding the CSV's and moving data around
+
+Region_csv <- select(Region_csv, State, Region)
+Resources_csv <- select(Resources_csv, State, 
+                        X..of.women.aged.18.49.using.contraceptives..2017..1.,
+                        Num_of_abortion_clinics_2017)
+Resources_Region_csv <- left_join(Resources_csv, 
+                                  Region_csv, by = "State")
+Resources_Region_csv <- head(Resources_Region_csv, -3)
+Resources_Region_csv <- select(Resources_Region_csv,Region,
+                          X..of.women.aged.18.49.using.contraceptives..2017..1.,
+                          Num_of_abortion_clinics_2017)
+
+#Making the number of clinics column numbers
+
+num_clinics <- as.numeric(Resources_Region_csv$Num_of_abortion_clinics_2017)
+Resources_Region_csv$num_clinics <- num_clinics
+
+#Grouping
+
+Number_Clinics <- Resources_Region_csv %>%
+  group_by(Region) %>%
+  summarise(Num_Clinics = sum(num_clinics))
+
+#Making the % of contraceptives column numbers
+
+Percentage_Contraceptives <- as.numeric
+(Resources_Region_csv$X..of.women.aged.18.49.using.contraceptives..2017..1.)
+Resources_Region_csv$Percentage_Contraceptives <- Percentage_Contraceptives
+
+#Grouping
+
+Percentage_Contraceptives <- Resources_Region_csv %>%
+  group_by(Region) %>%
+  summarise(Percent_Contraceptive_Use = round(mean(Percentage_Contraceptives,
+                                                   na.rm = TRUE)))
+
+#Combinding Number_Clinics and Percentage_Contraceptives
+
+Region_Contraceptives_Clinics <- left_join(Number_Clinics,
+                                           Percentage_Contraceptives,
+                                           by = "Region")
+
+#Scatterplot Graphing Function
+
+Percentage_Contraceptives_VS_Num_Clinics <- function(df){
+  
+  plot <- ggplot(data = df) + 
+    geom_point(mapping = aes(x = Num_Clinics,
+               y = Percent_Contraceptive_Use, color = Region),
+              size = 5)+labs(title = 
+              "% of Women Using Contraceptives vs Number of Abortion Clinics",
+              x = "Number of Abortion Clincs",
+              y = "% of Women Aged 18-49 Using Contraceptives") +
+              coord_cartesian(ylim = c(60, 80))
+  return(plot)
+  
+}
+
