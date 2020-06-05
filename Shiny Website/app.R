1c9c38ebd9c685d8cceb858118660abc7de94ad6
Kendyl Bouret <kendylb2@uw.edu>
2020-06-04 18:41:04 -0800

    fixed issues, now it loads :)

--- Shiny Website/app.R
+++ Shiny Website/app.R
@@ -1,3 +1,4 @@
+library("shiny")
 library("dplyr")
 library("ggplot2")
 library("gapminder")
@@ -8,6 +9,7 @@ source("app_ui.R")
 source("app_server.R")
 source("chart_three.R")
 source("chart_two.R")
+source("chart_one.R")
 
 
 
